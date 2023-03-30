a358ad7940ec2a2d9d8d825a1648776dcd4a589b
IvanSmiths <info@ivansmiths.com>
2023-03-30 16:31:05 +0200

    add jsx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,25 @@
+import { data } from "@/data/data"
+import Link from "next/link"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="perspective">
+        <div className="perspective-inner">
+          {data.map((project, index) =>
+            <div key={index} className={project.class}>
+              <img src={project.image} alt={project.text} />
+              <div className="perspective-desc">
+                <h2>{project.text}</h2>
+                <Link href="/">
+                  <span>Discover it</span>
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
