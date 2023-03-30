d4fb9cef99623dca49236f6cab2df904713e12b0
IvanSmiths <info@ivansmiths.com>
2023-03-30 16:23:15 +0200

    add array of projects

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        text: "Lorem ipsim dolor sit amet",
+        image: "project-1.jpg",
+        class: "perspective-left",
+    },
+    {
+        text: "Lorem ipsim dolor sit amet",
+        image: "project-2.jpg",
+        class: "perspective-right",
+    },
+    {
+        text: "Lorem ipsim dolor sit amet",
+        image: "project-3.jpg",
+        class: "perspective-left third",
+    },
+]
