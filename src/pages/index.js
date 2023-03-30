0ec7dacc95f0e435ac1a17b1d4dd8101a36149ea
IvanSmiths <info@ivansmiths.com>
2023-03-30 16:39:39 +0200

    referencing all elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import { data } from "@/data/data"
 import Link from "next/link"
+import { useRef } from "react";
 
 function Home() {
+  const perspectiveRef = useRef(null);
+  const triggerRef = useRef(null);
+  const refs = data.map(() => useRef([]));
+
   return (
     <div>
-      <div className="perspective">
-        <div className="perspective-inner">
-          {data.map((project, index) =>
-            <div key={index} className={project.class}>
+      <div ref={triggerRef} className="perspective">
+        <div ref={perspectiveRef} className="perspective-inner">
+          {data.map((project, index) => (
+            <div ref={refs[index]} key={index} className={project.class}>
               <img src={project.image} alt={project.text} />
               <div className="perspective-desc">
                 <h2>{project.text}</h2>
@@ -16,7 +21,7 @@ function Home() {
                 </Link>
               </div>
             </div>
-          )}
+          ))}
         </div>
       </div>
     </div>
